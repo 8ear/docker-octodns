ba8de4651a11136dd6ec7401b726ad8cbc595d4b
8ear <github@8ear.de>
2020-05-01 13:52:33 +0200

    Update Readme and change from Travis to Github Action

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# docker-octodns
+# Octodns Docker container
+| ||
+| --- | --- |
+| License: |![GitHub](https://img.shields.io/github/license/8ear/docker-wordpress)|
+| Github Workflow Build: | ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/8ear/docker-octodns/8earDockerCI) |
+| Docker Build Status: | ![Docker Build Status](https://img.shields.io/docker/cloud/build/8ear/octodns) |
+| Docker Automated build: | ![Docker Automated build](https://img.shields.io/docker/cloud/automated/8ear/octodns) |
+| Docker Pulls | ![Docker Pulls](https://img.shields.io/docker/pulls/8ear/octodns) |
+| Docker latest tag: | ![Docker Image Version (tag latest semver)](https://img.shields.io/docker/v/8ear/octodns) |
+
+
 Docker Container for OctoDNS https://github.com/github/octodns.
 This docker repository creates with the support of Travis the docker container in hub.docker.com.
-
-[![Build Status](https://travis-ci.org/8ear/docker-octodns.svg?branch=master)](https://travis-ci.org/8ear/docker-octodns)
 
 Docker Hub repository: https://hub.docker.com/r/8ear/octodns
 
--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-NAME="$DOCKER_USER/octodns"
+NAME=${NAME:-"test"}
 
 # build docker container
 
@@ -17,12 +17,31 @@ done
 docker build -t "$NAME:latest" -t "$NAME:stable" -f Dockerfile .
 docker build -t "$NAME:latest-full" -t "$NAME:stable-full" -f full/Dockerfile full/
 
-# Login to hub.docker.com
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
-# Push docker container
-CONTAINER_VERSION=`docker image ls 8ear/octodns --format "{{.Tag}}"`
-for V in $CONTAINER_VERSION
-do
-    docker push "$NAME:$V"
-done
+# Default value if var does not exist.
+DOCKER_USER=${DOCKER_USER:-"abc"}
+# Find all builded container
+CONTAINER_VERSION="$(docker image ls "$DOCKER_USER/$NAME" --format "{{.Tag}}")"
+
+
+# Push hub.docker.com
+if [[ ! -z "$DOCKER_PASS" ]] && [[ ! -z "$DOCKER_USER" ]]
+then
+    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+    for V in $CONTAINER_VERSION
+    do
+        docker push "$DOCKER_USER/$NAME:$V"
+    done
+fi
+
+
+# Push github repo
+if [[ ! -z "$GITHUB_PASS" ]] && [[ ! -z "$GITHUB_USER" ]]
+then
+    echo "$GITHUB_PASS" | docker login docker.pkg.github.com -u "$GITHUB_USER" --password-stdin
+    for V in $CONTAINER_VERSION
+    do
+        docker tag "$DOCKER_USER/$NAME:$V" "docker.pkg.github.com/${REPO}/$NAME:$V"
+        docker push "docker.pkg.github.com/${REPO}/$NAME:$V"
+    done
+fi
